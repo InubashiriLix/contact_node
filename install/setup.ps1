69551c7c737cc834ac35e8a00aacad229a84219b
Lix <lixinrong4002@gmail.com>
2025-01-19 19:47:24 +0800

    ignoring

--- install/setup.ps1
+++ install/setup.ps1
@@ -23,6 +23,8 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/root/GM/nuc/GMaster_project/src/sentry_msgs/install\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/root/GM/nuc/GMaster_project/src/contact/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
